dc078fb4d10857e3b4f669c8a5e85bf5fd78b299
Marci0224 <losonczim0224@gmail.com>
2026-02-19 10:16:06 +0100

    profile_start

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,9 +3,12 @@ import Navbar from './Navbar'
 import { useNavigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged, signOut} from "firebase/auth";
 import "./Profile.css";
+import { useState } from 'react';
 
 export default function Profile({auth}) {
 
+  const [username,setUsername]=useState("");
+  const [profilePicture,setProfilePicture]=useState("");
   
   const navigate = useNavigate()
 
@@ -25,6 +28,15 @@ export default function Profile({auth}) {
     <div className='profile'>
       <Navbar/>
       <div className='profileDiv'>
+        <div className="username">
+          <input type="text" value={username}/>
+          <input type="button" value="Névváltoztatás" />
+        </div>
+        <div className="profPicture">
+          <input type="text" value={profilePicture}/>
+          <input type="button" value="Profilkép változtatás" />
+        </div>
+
         <button onClick={()=>LogOut()}>Log out</button>
       </div>
     </div>
