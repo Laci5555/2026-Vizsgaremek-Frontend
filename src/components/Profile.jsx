7f490692d41b3b0af135657b0d366d92be5ca921
Marci0224 <losonczim0224@gmail.com>
2026-02-19 11:46:15 +0100

    edit_profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,32 +1,50 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
 import { useNavigate } from 'react-router-dom';
-import { getAuth, onAuthStateChanged, signOut} from "firebase/auth";
+import { signOut, updateProfile } from "firebase/auth";
 import "./Profile.css";
+import { FaUser } from 'react-icons/fa';
 
-export default function Profile({auth}) {
-
+export default function Profile({auth,darkmode,setDarkmode }) {
+  const [username, setUsername] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
   
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   async function LogOut() {
     try {
       await signOut(auth);
-      navigate("/login")
+      navigate("/login");
     } catch (err) {
       console.log(err);
     }
   }
 
-  
-
   return (
     <div className='profile'>
-      <Navbar/>
-      <div className='profileDiv'>
-        <button onClick={()=>LogOut()}>Log out</button>
+      <Navbar darkmode={darkmode} setDarkmode={setDarkmode}/>
+      <div className='profileContainer'>
+        <div className='profileCard'>
+          <h2>Edit profile</h2>
+          <div className="avatarPreview">
+            {profilePicture? <img src={profilePicture}/> : <img src="https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-600nw-2409187029.jpg"/>}
+          </div>
+
+          <div className="inputGroup">
+            <label>Username:</label>
+            <input type="text" placeholder="New username..." value={username} onChange={e => setUsername(e.target.value)} />
+          </div>
+
+          <div className="inputGroup">
+            <label>Profile picture URL:</label>
+            <input type="text" placeholder="Image url..." value={profilePicture} 
+              onChange={e => setProfilePicture(e.target.value)} />
+          </div>
+          <button className='saveBtn'>Save changes</button>
+          <hr />
+          <button className='logoutBtn' onClick={LogOut}>Logout</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
