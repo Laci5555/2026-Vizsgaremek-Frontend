6777d6fd521d37a96402ea7f82acc65fc8812fe6
Marci0224 <losonczim0224@gmail.com>
2026-03-05 11:36:47 +0100

    admin-skeleton

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,12 +1,57 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Admin() {
 
-    const genres=["Action","Adventure","Fighting","FPS","Gacha","Horror","MOBA","Puzzle","Racing","RPG","Strategy","Survival","Shooter","Simulation","Sports"];
+    //let genres=["Action","Adventure","Fighting","FPS","Gacha","Horror","MOBA","Puzzle","Racing","RPG","Strategy","Survival","Shooter","Simulation","Sports"];
+    let [gameName,setGameName]=useState("");
+    let [genres,setGenres]=useState(["Action","Adventure","Fighting","FPS","Gacha","Horror","MOBA","Puzzle","Racing","RPG","Strategy","Survival","Shooter","Simulation","Sports"]);
+    let [gameGenres,setGameGenres]=useState([]);
+    let [genre,setGenre]=useState("");
+    let [gamePicture,setGamePicture]=useState("");
+
+    function addGameGenres(item) {
+        let gameGenres2=[...gameGenres];
+        gameGenres2.push(item);
+        setGameGenres(gameGenres2);
+    }
+
+    function addGame() {
+        console.log(gameName);
+        console.log(gamePicture);
+    }
+
+    function addGenre() {
+        let genres2=[...genres];
+        genres2.push(genre);
+        setGenres(genres2);
+    }
+
+    console.log(gameGenres,genres);
+    
 
   return (
     <div className='admin'>
-      
+        <div className="addNewGame">
+            <div>
+                <label>Game name:</label>
+                <input type="text" value={gameName} onChange={(e) => setGameName(e.target.value)}/>
+            </div>
+            <div>
+                <label>Genres:</label>
+                <div>{genres.map(x=><div><input key={x} type="checkbox" name="" id="chk" onChange={()=>addGameGenres(x)}/><label htmlFor='chk'>{x}</label></div>)}</div>
+            </div>
+            <div>
+                <label>Game picture</label>
+                <input type="text" value={gamePicture} onChange={(e) => setGamePicture(e.target.value)}/>
+            </div>
+            <input type="button" value="Add genres to the new game" onClick={addGame}/>
+        </div>
+        <div className="addGenre">
+            <label>Genre:</label>
+            <input type="text" value={genre} onChange={(e) => setGenre(e.target.value)}/>
+            <input type="button" value="Add genre" onClick={addGenre}/>
+        </div>
     </div>
   )
 }
