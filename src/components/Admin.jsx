0bbe82b60601c34d7a2b23b3f9d97b791d5bb466
Marci0224 <losonczim0224@gmail.com>
2026-03-12 09:23:34 +0100

    Admin_page_changes

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
+import Navbar from './Navbar';
+import "./Admin.css";
 
 export default function Admin() {
 
@@ -10,6 +12,7 @@ export default function Admin() {
     let [gameGenres,setGameGenres]=useState([]);
     let [genre,setGenre]=useState("");
     let [gamePicture,setGamePicture]=useState("");
+    const [url,setUrl]=useState(true);
 
     function addGameGenres(item) {
         let gameGenres2=[...gameGenres];
@@ -20,6 +23,7 @@ export default function Admin() {
     function addGame() {
         console.log(gameName);
         console.log(gamePicture);
+        console.log(gameGenres);
     }
 
     function addGenre() {
@@ -28,31 +32,58 @@ export default function Admin() {
         setGenres(genres2);
     }
 
-    console.log(gameGenres,genres);
     
-
   return (
     <div className='admin'>
-        <div className="addNewGame">
-            <div>
-                <label>Game name:</label>
-                <input type="text" value={gameName} onChange={(e) => setGameName(e.target.value)}/>
+        <Navbar/>
+        <div className='adds'>
+            <div className="addNewGame">
+                <div>
+                    <label htmlFor='gameName'>Game name: </label>
+                    <input id='gameName' type="text" value={gameName} onChange={(e) => setGameName(e.target.value)}/>
+                </div>
+                <div>
+                    <span>Genres:</span>
+                    <div>{genres.map(x=><div key={x}><input  type="checkbox" name="" id="chk" onChange={()=>addGameGenres(x)}/><label htmlFor='chk'> {x}</label></div>)}</div>
+                </div>
+                <div className='pictureType'>
+                    <h2>Picture upload method</h2>
+                    <div className="toggle-wrapper">
+                        <span className={`label ${url ? "active" : ""}`}>With link</span>
+                        <label className="toggle">
+                            <input
+                                type="checkbox"
+                                checked={!url}
+                                onChange={(e) => setUrl(!e.target.checked)}
+                            />
+                            <div className="track">
+                                <div className="thumb" />
+                            </div>
+                        </label>
+                        <span className={`label ${!url ? "active" : ""}`}>With file</span>
+                    </div>
+                </div>
+                <div className='addPicture'>
+                    {url ? 
+                        <div>
+                            <label htmlFor='gamePicture'>Game picture: </label>
+                            <input id='gamePicture' type="text" value={gamePicture} onChange={(e) => setGamePicture(e.target.value)}/>
+                        </div> : 
+                        <div>
+                            <label for="myfile">Select files: </label>
+                            <input type="file" id="myfile" name="myfile" multiple value={gamePicture} onChange={(e) => setGamePicture(e.target.value)}/>
+                        </div>
+                    }
+                </div>
+                <input style={{width:"200px"}} type="button" value="Add new game" onClick={addGame}/>
             </div>
-            <div>
-                <label>Genres:</label>
-                <div>{genres.map(x=><div><input key={x} type="checkbox" name="" id="chk" onChange={()=>addGameGenres(x)}/><label htmlFor='chk'>{x}</label></div>)}</div>
+            <div className="addGenre">
+                <label htmlFor='genre'>Genre:</label>
+                <input id='genre' type="text" value={genre} onChange={(e) => setGenre(e.target.value)}/>
+                <input type="button" value="Add genre" onClick={addGenre}/>
             </div>
-            <div>
-                <label>Game picture</label>
-                <input type="text" value={gamePicture} onChange={(e) => setGamePicture(e.target.value)}/>
-            </div>
-            <input type="button" value="Add genres to the new game" onClick={addGame}/>
         </div>
-        <div className="addGenre">
-            <label>Genre:</label>
-            <input type="text" value={genre} onChange={(e) => setGenre(e.target.value)}/>
-            <input type="button" value="Add genre" onClick={addGenre}/>
-        </div>
+        
     </div>
   )
 }
